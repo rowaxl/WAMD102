ec55d11f6cfdc27bae656ec358098d6e342fa5ce
rowaxl <rowaxl0@gmail.com>
2020-05-25 17:01:36 -0700

    assignment 0525

---
+++ assignment/0525/factorial.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const fact = (number) => {
+  if (number === 1 || number === -1 || number === 0) {
+    return number;
+  }
+
+  let nextNum = 0;
+  if (number > 0) {
+    nextNum = number - 1;
+  } else {
+    nextNum = number + 1;
+  }
+
+  return number * fact(nextNum);
+}
+
+rl.question('Enter a number for calculate factorial: ', (number) => {
+  console.log(`${number}! = ${fact(parseInt(number))}`);
+
+  rl.close();
+});
---
+++ assignment/0525/factorial.py
@@ -0,0 +1,14 @@
+def fact(number):
+  if number == 1 or number == -1 or number == 0:
+    return number
+  
+  if number > 0:
+    nextNum = number - 1
+  else:
+    nextNum = number + 1
+
+  return number * fact(nextNum)
+
+number = int(input("Enter a number for calculate factorial: "))
+
+print(f"{number}! = ", fact(number))
